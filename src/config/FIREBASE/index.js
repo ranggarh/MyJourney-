4b5dade82f5bcb0c89c6ccf641bda2c5f8661a32
Albaar825 <firdausalbar123@gmail.com>
2023-12-10 19:24:42 +0700

    Update Login Register Firebase

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/database";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBF1VshoaLcYX_6d34EwFSPsP9Y2rgAPag",
+    authDomain: "myjourney-a8577.firebaseapp.com",
+    projectId: "myjourney-a8577",
+    storageBucket: "myjourney-a8577.appspot.com",
+    messagingSenderId: "506064702655",
+    appId: "1:506064702655:web:6dfb5d2bee65aa8260ca0a"
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
